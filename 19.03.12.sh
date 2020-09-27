5cc1f15e926de682c94641dd8a8e54ba54309e70
Martice Nicks <martice.nicks@maxar.com>
2020-09-27 11:23:44 -0400

    Update 19.03.12.sh

--- 19.03.12.sh
+++ 19.03.12.sh
@@ -411,7 +411,7 @@ do_install() {
 					echo "Info: Enabling channel '$CHANNEL' for docker-ce repo"
 					$sh_c "$config_manager $enable_channel_flag docker-ce-$CHANNEL"
 				fi
-				$sh_c "sed -i 's/\$releasever/7/g' /etc/yum.repos.d/docker-ce.repo"
+				sh -c "sed -i 's/\$releasever/7/g' /etc/yum.repos.d/docker-ce.repo"
 				$sh_c "$pkg_manager makecache fast"
 				
 				$sh_c "$pkg_manager install -y -q docker-ce-${docker_version} docker-ce-cli-${docker_version}"
